c798d81b4101b0da49539fd7d3bb0d45f6f648f8
Chirakid Kaweesirikon <chirakid.k@gmail.com>
2019-04-19 23:23:34 +0700

    update

--- src/test.js
+++ src/test.js
@@ -11,34 +11,34 @@ class App extends Component {
                     </div>
                     <div className="text-center">
                         <i className="fab fa-facebook-square fa-3x ContactIcon" />
+                        <i className="fab fa-line fa-3x ContactIcon" />
                     </div>
-                    
                     <div>
                         <div className="Title">Profile</div>
                         <div>
                             <div>Chirakid Kaweesirikon [Aum]</div>
-                            <div>Email : aum.chirakid@gmail.com</div>
-                            <div>Phone : 0827639234</div>
                         </div>
                     </div>
                     <div>
                         <div className="Title">Technical skills</div>
                         <div>
-                            <div>Html</div>
-                            <div className="progress">
-                                <div className="progress-bar" style={{ width: "70%" }} aria-valuemin="0" aria-valuemax="100"></div>
+                            <div>
+                                <div>Html</div>
+                                <div className="progress">
+                                    <div className="progress-bar" style={{ width: "70%" }} aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
                             </div>
-                        </div>
-                        <div>
-                            <div>Js</div>
-                            <div className="progress">
-                                <div className="progress-bar" style={{ width: "50%" }} aria-valuemin="0" aria-valuemax="100"></div>
+                            <div>
+                                <div>Js</div>
+                                <div className="progress">
+                                    <div className="progress-bar" style={{ width: "50%" }} aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
                             </div>
-                        </div>
-                        <div>
-                            <div>PHP</div>
-                            <div className="progress">
-                                <div className="progress-bar" style={{ width: "70%" }} aria-valuemin="0" ariaCaluemax="100"></div>
+                            <div>
+                                <div>PHP</div>
+                                <div className="progress">
+                                    <div className="progress-bar" style={{ width: "70%" }} aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -48,18 +48,22 @@ class App extends Component {
                         <div className="Title">Experience</div>
                         <div>
                             <div className="SubTitle">Web Dev</div>
-                            <ul>
-                                <li>SHIPPOP - 2018 : Web application</li>
-                            </ul>
+                            <div>
+                                <ul>
+                                    <li>SHIPPOP - 2018 : Web application</li>
+                                </ul>
+                            </div>
                         </div>
                         <div>
                             <div className="SubTitle">Internship - 2017</div>
-                            <ul>
-                                <li>Learned Html</li>
-                                <li>Learned Css/Scss</li>
-                                <li>Learned Js</li>
-                                <li>Learned PHP</li>
-                            </ul>
+                            <div>
+                                <ul>
+                                    <li>Html</li>
+                                    <li>Css/Scss</li>
+                                    <li>Js</li>
+                                    <li>PHP</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                     <div>
